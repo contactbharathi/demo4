7d3ce500a6d5b336a5d6debab86e5f4657d0a6ce
Kavitha12543 <bharathicontact@yahoo.co.in>
2019-03-12 22:07:51 +0000

    removed idea

--- src/test/java/grpid4/RunCucumberClass.java
+++ src/test/java/grpid4/RunCucumberClass.java
@@ -13,6 +13,6 @@ import org.junit.runner.RunWith;
         strict = false,
         plugin = {"pretty","json:target/cucumber.json"})
 
-public class RunCucumberClasstest
+public class RunCucumberClass
 {
 }
